417c8c2115b93526aa185373668b10027afe2388
Rajesh <venkata.nakka@brother.com>
2025-07-27 15:16:31 +0530

    RBAC changes

--- WebApplication1/WebApplication1/obj/Debug/net8.0/RBAC.Api.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/RBAC.Api.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RBAC.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de4ab6f6b4cf2134061afedff9239c2fa017152")]
 [assembly: System.Reflection.AssemblyProductAttribute("RBAC.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RBAC.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
